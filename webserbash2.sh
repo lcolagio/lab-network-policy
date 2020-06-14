b0d58b870fca03608066ec66314aaefecfc44def
lcolagio <66462164+lcolagio@users.noreply.github.com>
2020-06-14 20:27:33 +0200

    Update webserbash2.sh

--- webserbash2.sh
+++ webserbash2.sh
@@ -1,1 +1,1 @@
-while true; do { cat index.html ; hostname; ip a | grep 'global eth0'}  | nc -l 8080; done
+while true; do { cat index.html ; hostname; ip a | grep 'global eth0' ; }  | nc -l 8080; done
